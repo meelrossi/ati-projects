a7a4f3bafbd25415c26ed5ad8ccc65aad0868df8
Nicolas Di Nucci <nico.dinu@gmail.com>
2017-03-26 23:55:47 -0700

    Adds NoiseGenerators

---
+++ src/noise/GaussianNoiseGenerator.java
@@ -0,0 +1,24 @@
+package noise;
+
+import random_number_generator.GaussianRandomNumberGenerator;
+
+public class GaussianNoiseGenerator extends NoiseGenerator{
+	
+	private final GaussianRandomNumberGenerator gaussianRng;
+	
+	public GaussianNoiseGenerator(double mean, double standardDeviation) {
+		this.gaussianRng = new GaussianRandomNumberGenerator(mean,standardDeviation);
+	}
+	
+	@Override
+	public int addNoiseToPixel(int pixel) {
+		pixel += gaussianRng.random();
+		if(pixel<0){
+			return 0;
+		} else if(pixel>255){
+			return 255;
+		}
+		return (int)pixel;
+	}
+
+}
---
+++ src/noise/SaltAndPepperNoiseGenerator.java
@@ -0,0 +1,33 @@
+package noise;
+
+import random_number_generator.UniformRandomNumberGenerator;
+
+public class SaltAndPepperNoiseGenerator extends NoiseGenerator {
+
+	private final UniformRandomNumberGenerator uniformRng;
+	private final double p1;
+	private final double p2;
+	
+	
+	public SaltAndPepperNoiseGenerator(double p1, double p2) {
+		this.p1 = p1;
+		this.p2 = p2;
+		this.uniformRng = new UniformRandomNumberGenerator();
+	}
+
+
+	@Override
+	public int addNoiseToPixel(int pixel) {
+		double p = uniformRng.random();
+		if(p1>p){
+			return 0;
+		} else if(p2<p){
+			return 255;
+		} else{
+			return pixel;
+		}
+	}
+	
+	
+
+}
