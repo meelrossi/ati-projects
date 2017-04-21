034cbb68e502f6417fc557590ba00511a6f973cf
Nicolas Di Nucci <nico.dinu@gmail.com>
2017-04-21 20:33:29 -0300

    mask applied to border filter

--- src/filter/BorderFilter.java
+++ src/filter/BorderFilter.java
@@ -1,43 +1,29 @@
 package filter;
 
+import matrix_operations.Mask;
 import model.ColorImage;
 
 public class BorderFilter extends Filter {
-
+	
 	@Override
 	public ColorImage filter(ColorImage img, int windowSize) {
 		int width = img.getWidth();
 		int height = img.getHeight();
-		double[][] red = new double[width][height];
-		double[][] green = new double[width][height];
-		double[][] blue = new double[width][height];
-		for (int i = 0; i < width; i++) {
-			for (int j = 0; j < height; j++) {
-				red[i][j] = borderFilter(img.getRedChannel(), i, j, width, height, windowSize);
-				green[i][j] = borderFilter(img.getGreenChannel(), i, j, width, height, windowSize);
-				blue[i][j] = borderFilter(img.getBlueChannel(), i, j, width, height, windowSize);
+		
+		double[][] maskMatrix = new double[windowSize][windowSize];
+		for(int i = 0; i < windowSize; i++){
+			for(int j = 0; j < windowSize; j++){
+				maskMatrix[i][j] = -1;
 			}
 		}
+		maskMatrix[(windowSize+1)/2][(windowSize+1)/2] = windowSize*windowSize-1;
+		Mask mask = new Mask(maskMatrix, 0);
+		double[][] red = mask.apply(img.getRedChannel());
+		double[][] green = mask.apply(img.getGreenChannel());
+		double[][] blue = mask.apply(img.getBlueChannel());
 		ColorImage result = new ColorImage(red, green, blue, width, height);
 		result.normalize();
 		return result;
-	}
-	
-	public double borderFilter(double[][] channel, int x, int y, int width, int height, int windowSize) {
-		double sum = 0;
-		int move = (windowSize - 1) / 2;
-		int count = 0;
-		for (int i = x - move; i <= x + move; i++) {
-			for (int j = y - move; j <= y + move; j++) {
-				if (i >= 0 && i < width && j >= 0 && j < height && !(x == i && y == j)) {
-					sum -= channel[i][j];
-					count ++;
-				}
-			}
-		}
-		sum += count * channel[x][y];
-		return sum;
-		
 	}
 
 }
--- src/matrix_operations/Mask.java
+++ src/matrix_operations/Mask.java
@@ -2,28 +2,29 @@ package matrix_operations;
 
 public class Mask {
 
-	private int[][] mask;
-	private int default_value = 0;
+	private double[][] mask;
+	private double default_value = 0;
 	private int mx;
 	private int my;
 	
 	
-	public Mask(int[][] mask){
+	public Mask(double[][] mask){
 		this.mask = mask;
-		this.mx = (mask[0].length-1)/2;
-		this.my = (mask.length-1)/2;
+		this.mx = (mask.length-1)/2;
+		this.my = (mask[0].length-1)/2;
 	}
 	
-	public Mask(int[][] mask, int default_value){
+	public Mask(double[][] mask, int default_value){
 		this.mask = mask;
+		this.mx = (mask.length-1)/2;
+		this.my = (mask[0].length-1)/2;
 		this.default_value = default_value;
 	}
 	
-	public int[][] apply(int[][] matrix){
-		int[][] result = new int[matrix[0].length][matrix.length];
-		
-		for(int i=0; i<matrix[0].length; i++){
-			for(int j=0; j<matrix.length; j++){
+	public double[][] apply(double[][] matrix){
+		double[][] result = new double[matrix.length][matrix[0].length];
+		for(int i=0; i<matrix.length; i++){
+			for(int j=0; j<matrix[0].length; j++){
 				if(maskDoesNotFit(matrix, i, j)){
 					result[i][j] = default_value;
 				} else {
@@ -34,7 +35,7 @@ public class Mask {
 		return result;
 	}
 
-	private int applyMaskToPoint(int[][] matrix, int i, int j) {
+	private int applyMaskToPoint(double[][] matrix, int i, int j) {
 		int sum = 0;
 		for(int x = -mx; x <= mx; x++){
 			for(int y = -my; y <= my; y++){
@@ -44,10 +45,10 @@ public class Mask {
 		return sum;
 	}
 
-	private boolean maskDoesNotFit(int[][] matrix, int i, int j) {
-		int minimunX = (mask[0].length-1)/2;
-		int minimunY = (mask.length-1)/2;
-		return i<minimunX || j<minimunY || i>=matrix[0].length-minimunX || j>=matrix.length-minimunY;
+	private boolean maskDoesNotFit(double[][] matrix, int i, int j) {
+		int minimunX = mx;
+		int minimunY = my;
+		return i<minimunX || j<minimunY || i>=matrix.length-minimunX-1 || j>=matrix[0].length-minimunY-1;
 	}
 	
 }
